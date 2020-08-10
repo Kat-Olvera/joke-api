aeefda30b13224759e0231085a96229d8fdea9c8
jose-Olvera <abraham.jolvera@gmail.com>
2020-08-10 00:10:51 -0700

    need to figure this out more

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-const app  = document.getElementById('root');
-const logo = document.createElement('img');
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+// const app  = document.getElementById('root');
+// const logo = document.createElement('img');
+// const container = document.createElement('div');
+// container.setAttribute('class', 'container');
 
-app.appendChild(logo);
-app.appendChild(container);
+// app.appendChild(logo);
+// app.appendChild(container);
 
 
 
@@ -12,46 +12,47 @@ app.appendChild(container);
 const joke_api_url = 'https://official-joke-api.appspot.com/random_joke';
 
 async function getJoke(){
-    
+    // const app = document.getElementById('root')
+    // getting and parsing the data
     const response = await fetch(joke_api_url);
     const data = await response.json();
     const { setup, punchline } = data;
 
-   document.getElementById('setup-element').textContent = setup;
-   document.getElementById('punch-element').textContent = punchline;
+
+    //grabbing the spans that will hold setup and punchline
+//    document.getElementById('setup-element').textContent = setup;
+//    document.getElementById('punch-element').textContent = punchline;
+
+    //creating and setting classes to new elements that will appear after you press the button
+    const jokeBoxH1 = document.createElement('h1')
+
+    const setupSpan = document.createElement('span')
+    setupSpan.setAttribute('class', 'setup')
+
+    const punchlineSpan = document.createElement('span')
+    punchlineSpan.setAttribute('class', 'punchline')
+
+    //putting the api data into the elements i just made
+    setupSpan.textContent = data.setup
+    punchlineSpan.textContent = data.punchline
+
+    //appending those elements to the html
+    document.body.appendChild(jokeBoxH1)
+    document.body.jokeBoxH1.appendChild(setupSpan)
+
+
 };
 
-
-
-
-getJoke();
-
+document.querySelector('#jokebutton').addEventListener('click', getJoke())
 
 
 
 
 
 
-// const request = new XMLHttpRequest();
 
-// // request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-// request.open('GET', 'https://official-joke-api.appspot.com/random_joke', true);
 
-// request.onload = function () {
-//     //begin accessing json here
-//     const data = JSON.parse(this.response); // json is now parsed in the variable data
-    
-//     if (request.status >= 200 && request.status < 400) {
-         
-//         for(let i = 0; i < data.length; i++){
-//             console.log(data.textContent)
-            
-//         }
-        
-//       } else {
-//         console.log('error')
-//       }
-    
 
-// }
-// request.send();
+
+// document.getElementById('setup-element').textContent = setup;
+// document.getElementById('punch-element').textContent = punchline;
