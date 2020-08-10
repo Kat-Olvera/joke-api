8a1a63420bd2e9634dd22aeabf65be74bb0c088a
jose-Olvera <abraham.jolvera@gmail.com>
2020-08-10 01:44:17 -0700

    makes joke but creates a new element everytime

--- scripts.js
+++ scripts.js
@@ -1,24 +1,12 @@
-// const app  = document.getElementById('root');
-// const logo = document.createElement('img');
-// const container = document.createElement('div');
-// container.setAttribute('class', 'container');
-
-// app.appendChild(logo);
-// app.appendChild(container);
-
-
 
 const randomJokeElement = document.querySelector('.random-joke')
 const joke_api_url = 'https://official-joke-api.appspot.com/random_joke';
 
 async function getJoke(){
-    const response = await fetch(joke_api_url); // getting and parsing the data
+    const response = await fetch(joke_api_url); 
     const data = await response.json();
 
-//    document.getElementById('setup-element').textContent = setup;  //grabbing the spans that will hold setup and punchline
-//    document.getElementById('punch-element').textContent = punchline;
-    
-    const jokeBoxH1 = document.createElement('h1') //new elements that will appear after you press the button
+    const jokeBoxH1 = document.createElement('h1') 
 
     const setupSpan = document.createElement('span')
     setupSpan.setAttribute('class', 'setup')
@@ -27,11 +15,11 @@ async function getJoke(){
     punchlineSpan.setAttribute('class', 'punchline')
 
     
-    const joke = data.setup + ' ' + data.punchline //adding the two values i need into a variable for ease of use
-    jokeBoxH1.textContent = joke //making the h1 element i made the content of the api request
+    const joke = data.setup + ' ' + data.punchline 
+    jokeBoxH1.textContent = joke 
    
 
-    //appending those elements to the html
+    
     document.body.appendChild(jokeBoxH1)
     jokeBoxH1.appendChild(randomJokeElement)
    
@@ -39,14 +27,3 @@ async function getJoke(){
 };
 
 document.querySelector('#jokebutton').addEventListener('click',getJoke)
-
-
-
-
-
-
-
-
-
-// document.getElementById('setup-element').textContent = setup;
-// document.getElementById('punch-element').textContent = punchline;
