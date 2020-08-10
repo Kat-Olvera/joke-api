5bfa06413ed7e516027a083452bcab6aad4b1831
jose-Olvera <abraham.jolvera@gmail.com>
2020-08-10 02:13:26 -0700

    best version

--- scripts.js
+++ scripts.js
@@ -1,29 +1,24 @@
 
-const randomJokeElement = document.querySelector('.random-joke')
-const joke_api_url = 'https://official-joke-api.appspot.com/random_joke';
+const randomJokeElement = document.querySelector('.random-joke') // element that will hold the actual joke 
+const joke_api_url = 'https://official-joke-api.appspot.com/random_joke'; // api url variable to fetch at later
 
 async function getJoke(){
-    const response = await fetch(joke_api_url); 
-    const data = await response.json();
+    const response = await fetch(joke_api_url); //getting api
+    const data = await response.json();  // parsing data?
 
-    const jokeBoxH1 = document.createElement('h1') 
-
-    const setupSpan = document.createElement('span')
-    setupSpan.setAttribute('class', 'setup')
-
-    const punchlineSpan = document.createElement('span')
-    punchlineSpan.setAttribute('class', 'punchline')
-
+    randomJokeElement.innerHTML = '' // so after the function is recalled by pressing again the page will delete the previous element
     
-    const joke = data.setup + ' ' + data.punchline 
-    jokeBoxH1.textContent = joke 
+    const jokeBoxH1 = document.createElement('h1') //the h1 with the joke
+    
+    const joke = data.setup + ' ' + data.punchline // variable to hold the joke content easier
+    jokeBoxH1.textContent = joke // using that variable and changing the text content to the api data
    
 
     
-    document.body.appendChild(jokeBoxH1)
-    jokeBoxH1.appendChild(randomJokeElement)
+    document.body.appendChild(randomJokeElement) //append the section that will hold joke to the document body
+    randomJokeElement.appendChild(jokeBoxH1) //then append the h1 to the last element randomJokeElement
    
 
 };
 
-document.querySelector('#jokebutton').addEventListener('click',getJoke)
+document.querySelector('#jokebutton').addEventListener('click',getJoke) // select the button and on click run the fucntion
